85f8e8f26e75aa0a6802401883cc34ece055269a
Thomas Johander <lopan@ThomasJndersMBP.example.com>
2017-02-18 08:37:42 -0600

    Array values writing to the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,6 +7,18 @@ $(document).ready(function(){
       success: function(data){
         // yay! we have data!
         console.log('returned data from server: ', data);
+        console.log('the length of the array I found is: ',data.phirephiters.length);
+        writeShoutOutsToDom(data.phirephiters);
       }
     });
 });
+
+//append the DOM to write out the key values for each object in the source array
+function writeShoutOutsToDom(array){
+  for (var i = 0; i < array.length; i++) {
+    var minimizedName = array[i].name.replace(/\s/g,''); //remove spaces in array[i].name key values to use in HTML id attrtibutes
+    $('#thePhirephitersList').append('<p class="name" id="'+minimizedName+'">name: '+array[i].name+'<p>');
+    $('#thePhirephitersList').append('<p class="shoutout" id="'+array[i].git_username+'">git_username: '+array[i].git_username+'<p>');
+    $('#thePhirephitersList').append('<p class="shoutout">shoutout: '+array[i].shoutout+'<p>');
+  }
+}
